89beb644c2a3fba961388703783cc75d9f37945d
Yarley Tique <arley.skn@gmail.com>
2025-01-16 19:55:40 +0000

    prueba

--- src/app.js
+++ src/app.js
@@ -5,7 +5,23 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+var datos = [];
+const guardarCampeon = document.getElementById("guardarCampeon");
+guardarCampeon.addEventListener("click", guardarDatos);
+function guardarDatos() {
+  let campeonDatos = document.getElementById("campeon").value;
+  let lineaDatos = document.getElementById("linea").value;
+  let hechizoDatos = document.getElementById("hechizo").value;
+
+  let nuevoDato = {
+    campeon: campeonDatos,
+    linea: lineaDatos,
+    hechizo: hechizoDatos
+  };
+
+  datos.push(nuevoDato);
+
+  document.getElementById("campeon").value = "";
+  document.getElementById("linea").value = "";
+  document.getElementById("hechizo").value = "";
+}
